c14f97f7d312dd0c2be728d7a5d1542ebb2db201
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2023-01-22 15:48:49 -0300

    CategoryDTO e métodos CRUD (faltou o delete) OK

--- src/main/java/com/jameseng/workshop/resources/CategoryResource.java
+++ src/main/java/com/jameseng/workshop/resources/CategoryResource.java
@@ -1,14 +1,13 @@
 package com.jameseng.workshop.resources;
 
-import com.jameseng.workshop.entities.Category;
+import com.jameseng.workshop.dto.CategoryDTO;
 import com.jameseng.workshop.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,14 +18,28 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> category = categoryService.findAll();
-        return ResponseEntity.ok().body(category);
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> categoryDto = categoryService.findAll();
+        return ResponseEntity.ok().body(categoryDto);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Category> findById(@PathVariable Long id) {
-        Category category = categoryService.findById(id);
-        return ResponseEntity.ok().body(category);
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO categoryDto = categoryService.findById(id);
+        return ResponseEntity.ok().body(categoryDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO categoryDto) {
+        categoryDto = categoryService.insert(categoryDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(categoryDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(categoryDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO categoryDto) {
+        categoryDto = categoryService.update(id, categoryDto);
+        return ResponseEntity.ok().body(categoryDto);
     }
 }
--- src/main/java/com/jameseng/workshop/services/CategoryService.java
+++ src/main/java/com/jameseng/workshop/services/CategoryService.java
@@ -1,12 +1,16 @@
 package com.jameseng.workshop.services;
 
+import com.jameseng.workshop.dto.CategoryDTO;
 import com.jameseng.workshop.entities.Category;
 import com.jameseng.workshop.repositories.CategoryRepository;
+import com.jameseng.workshop.services.exeptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -14,12 +18,38 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
     }
 
-    public Category findById(Long id) {
-        Optional<Category> category = categoryRepository.findById(id);
-        return category.get();
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+                "CategoryService/Entity not found. id = " + id));
+        //return category.get();
+        return new CategoryDTO(category);
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO categoryDto) {
+        Category category = new Category();
+        category.setName(categoryDto.getName());
+        category = categoryRepository.save(category);
+        return new CategoryDTO(category);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO categoryDto) {
+        try {
+            Category category = categoryRepository.getReferenceById(id);
+            category.setName(categoryDto.getName());
+            category = categoryRepository.save(category);
+            return new CategoryDTO(category);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("CategoryService/Entity not found. id = " + id);
+        }
+    }
+
 }
