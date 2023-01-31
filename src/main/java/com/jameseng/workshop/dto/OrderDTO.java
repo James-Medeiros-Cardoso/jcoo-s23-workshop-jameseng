d3fead427bf73c30f2cb40a240be83e94498e632
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2023-01-31 19:17:59 -0300

    trabalhando na OrderDTO

--- src/main/java/com/jameseng/workshop/dto/OrderDTO.java
+++ src/main/java/com/jameseng/workshop/dto/OrderDTO.java
@@ -21,29 +21,38 @@ public class OrderDTO {
 
     private Set<OrderItemDTO> items = new HashSet<>();
 
+    private Double total;
+
     private Payment payment;
 
     public OrderDTO() {
 
     }
 
-    public OrderDTO(Long id, Instant moment, Integer orderStatus, UserDTO client, Set<OrderItemDTO> items, Payment payment) {
+    public OrderDTO(Long id, Instant moment, Integer orderStatus, UserDTO client, Set<OrderItemDTO> items, Double total, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.orderStatus = orderStatus;
         this.client = client;
         this.items = items;
+        this.total = total;
         this.payment = payment;
     }
 
-    public OrderDTO(Order order, Set<OrderItem> orderItem) {
+    public OrderDTO(Order order) {
         id = order.getId();
         moment = order.getMoment();
         orderStatus = order.getOrderStatus().getCode();
         client = new UserDTO(order.getClient());
+        total = order.getTotal();
+        payment = order.getPayment();
+    }
+
+    public OrderDTO(Order order, Set<OrderItem> orderItem) {
+        this(order);
         //setItems(orderItemDto.stream().map(oi -> new OrderItemDTO(oi)).collect(Collectors.toList()));
         //setItems(orderItem.stream().map(x -> new OrderItemDTO(x)).collect(Collectors.toList()););
-        payment = order.getPayment();
+        //this.items.stream().map(oi -> new OrderItemDTO(oi)).collect(Collectors.toList());
     }
 
     public Long getId() {
@@ -84,6 +93,14 @@ public class OrderDTO {
 
     public void setItems(Set<OrderItemDTO> items) {
         this.items = items;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
     }
 
     public Payment getPayment() {
